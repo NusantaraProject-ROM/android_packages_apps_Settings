2a86898a3446ece5bb79d0e57bc0846b2e4525c4
Joe Maples <joe@frap129.org>
2017-09-08 14:05:31 -0500

    Settings: Integrate Nusantara Wings into Settings
    Based on both DU's Nougat and Omni's Oreo implimentations, so
    credits to mazda and maxwen for this.
    
    Change-Id: Id4e5367bab5826bfc8bced5684fc2e599f8140c4
    Signed-off-by: Joe Maples <joe@frap129.org>
    [jaysonedson: Ported to android11 and Android.bp]
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -240,4 +240,8 @@ public class Settings extends SettingsActivity {
      */
     public static class MediaControlsSettingsActivity extends SettingsActivity {}
 
+    /**
+     * Activity for Nusantara Wings
+     */
+    public static class NusantaraWingsActivity extends SettingsActivity { /* empty */ }
 }
