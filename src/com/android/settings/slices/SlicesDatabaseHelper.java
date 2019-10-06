82290b99198933f89053f991c912f774ed5525ec
maxwen <max.weninger@gmail.com>
2019-10-06 15:46:05 -0400

    Use BUILD.DATE for search index check
    Change-Id: I7199d9f812d8cb645f3abca815c62098248b53c4
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- src/com/android/settings/slices/SlicesDatabaseHelper.java
+++ src/com/android/settings/slices/SlicesDatabaseHelper.java
@@ -235,6 +235,6 @@ public class SlicesDatabaseHelper extends SQLiteOpenHelper {
 
     @VisibleForTesting
     String getBuildTag() {
-        return Build.NAD_FINGERPRINT;
+        return Build.DATE;
     }
 }
