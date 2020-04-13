542edcc340a1a78ada65f1180c4885cf94d859f5
Satheesh Kumar Pallemoni <spallemo@codeaurora.org>
2020-04-13 20:18:09 +0530

    bluetooth: test fixments /do not merge/

--- src/com/android/settings/bluetooth/SavedBluetoothTwsDeviceUpdater.java
+++ src/com/android/settings/bluetooth/SavedBluetoothTwsDeviceUpdater.java
@@ -16,6 +16,7 @@
  *    * Neither the name of The Linux Foundation nor the names of its
  *      contributors may be used to endorse or promote products derived
  *      from this software without specific prior written permission.
+
  * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
  * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
  * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
