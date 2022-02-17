57c467db941200506b2857e3a1081bcd78fa4688
jhonboy121 <alfredmathew05@gmail.com>
2022-02-18 01:49:38 +0530

    Settings: rewrite refresh rate preference controllers in kt and improve
    * Added settings observer to update preference on settings changes. We have a tile so it would
      be nice to reflect changes when tile state is changed.
    * Restrict max refresh rate list based on the min rate set since it is absurd to allow setting max
      values below current min rate.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -25,6 +25,8 @@ import com.android.settings.development.OverlayCategoryPreferenceController;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
+import com.android.settings.display.MaxRefreshRatePreferenceController;
+import com.android.settings.display.MinRefreshRatePreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
@@ -85,6 +87,8 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new EmulateDisplayCutoutPreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new MinRefreshRatePreferenceController(context, lifecycle));
+        controllers.add(new MaxRefreshRatePreferenceController(context, lifecycle));
         return controllers;
     }
 
